82e5268f8c75967a71027a7c8e2e9f8da1b0ea8a
degaart <zorro@zorro.ovh>
2022-05-18 21:34:46 +0300

    Initial import

---
+++ src/ffi.rs
@@ -0,0 +1,37 @@
+struct StrConverter {
+    data: CString,
+}
+
+impl StrConverter {
+    pub fn new(s: impl AsRef<str>) -> Result<Self> {
+        let data = CString::new(s.as_ref())?;
+        Ok(Self { data })
+    }
+
+    pub fn as_c_char(&self) -> *const c_char {
+        self.data.as_ptr() as *const c_char
+    }
+}
+
+struct PathConverter {
+    data: CString,
+}
+
+impl PathConverter {
+    pub fn new(s: impl AsRef<Path>) -> Result<Self> {
+        let as_string = s.as_ref().to_str();
+        if let None = as_string {
+            bail!("Invalid path");
+        }
+
+        let data = CString::new(as_string.unwrap())?;
+        Ok(Self { data })
+    }
+
+    pub fn as_c_char(&self) -> *const c_char {
+        self.data.as_ptr() as *const c_char
+    }
+}
+
+
+
